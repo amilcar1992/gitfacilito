0cd438277562b053b26242c285d545308b5c3c56
amilcar1992 <amilcarrada1992@gmail.com>
2016-11-01 13:37:22 -0300

    Todos Modificados

---
+++ tabla de multiplicar.rb
@@ -0,0 +1,13 @@
+puts "ingrese primer dato"
+ dato1texto = gets.chomp
+ dato1= dato1texto.to_i
+puts "ingrese segundo dato" 
+dato2texto = gets.chomp
+dato2 =dato2texto.to_i
+resultado = 0 
+if dato2 == "1"
+resultado = dato1*dato2
+else
+end
+puts"resultado"
+puts resultado
